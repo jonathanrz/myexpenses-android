dceae7b417f0d195a199db815dd79393b77f406e
Jonathan Zanella <jonathanrafaelz@gmail.com>
2016-06-21 23:43:30 -0300

    Using auth token to connect to server

--- app/src/main/java/br/com/jonathanzanella/myexpenses/server/Server.java
+++ app/src/main/java/br/com/jonathanzanella/myexpenses/server/Server.java
@@ -3,7 +3,14 @@ package br.com.jonathanzanella.myexpenses.server;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+
+import br.com.jonathanzanella.myexpenses.BuildConfig;
 import br.com.jonathanzanella.myexpenses.Environment;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -12,23 +19,37 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by jzanella on 6/5/16.
  */
 public class Server {
-    private Retrofit retrofit;
+	private Retrofit retrofit;
 
-    public Server() {
-        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+	private class HeaderInterceptor implements Interceptor {
+		@Override
+		public Response intercept(Chain chain)
+				throws IOException {
+			Request request = chain.request();
+			request = request.newBuilder()
+					.addHeader("Auth-token", BuildConfig.MYEXPENSES_AUTH_TOKEN)
+					.build();
+			return chain.proceed(request);
+		}
+	}
 
-        retrofit = new Retrofit.Builder()
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create(gson))
+	public Server() {
+		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 
-                .baseUrl(Environment.SERVER_URL)
-                .build();
-    }
+		OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new HeaderInterceptor()).build();
 
-    public SourceInterface sourceInterface() {
-        return retrofit.create(SourceInterface.class);
-    }
-    public AccountInterface accountInterface() {
-        return retrofit.create(AccountInterface.class);
-    }
+		retrofit = new Retrofit.Builder()
+				.addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+				.addConverterFactory(GsonConverterFactory.create(gson))
+				.baseUrl(Environment.SERVER_URL)
+				.client(client)
+				.build();
+	}
+
+	public SourceInterface sourceInterface() {
+		return retrofit.create(SourceInterface.class);
+	}
+	public AccountInterface accountInterface() {
+		return retrofit.create(AccountInterface.class);
+	}
 }
