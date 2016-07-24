81066a7f9e7c0926e95a95c3ad8657c5688f82e1
Jonathan Zanella <jonathanrafaelz@gmail.com>
2016-07-24 15:52:53 -0300

    Creating travis ci script

---
+++ .travis_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ev
+./gradlew assembleDebug
+./gradlew connectedAndroidTest
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+	./gradlew testfairyJonathan
+	./gradlew testfairyThainara
+fi
