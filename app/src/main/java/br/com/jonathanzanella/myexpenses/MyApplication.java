96d5558f05835e50e4f5963268c1e186fee6ab9e
Jonathan Zanella <jonathanrafaelz@gmail.com>
2016-08-07 19:05:54 -0300

    merge

--- app/src/main/java/br/com/jonathanzanella/myexpenses/MyApplication.java
+++ app/src/main/java/br/com/jonathanzanella/myexpenses/MyApplication.java
@@ -9,6 +9,7 @@ import com.raizlabs.android.dbflow.config.FlowManager;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
+import br.com.jonathanzanella.myexpenses.database.MyDatabase;
 import lombok.Getter;
 
 /**
@@ -18,14 +19,21 @@ import lombok.Getter;
 public class MyApplication extends Application {
 	@Getter
 	private static Context context;
+	@Getter
+	private static MyApplication application;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		MyApplication.application = this;
 		MyApplication.context = getApplicationContext();
 
 		FlowManager.init(new FlowConfig.Builder(this).build());
 		JodaTimeAndroid.init(this);
 		Stetho.initializeWithDefaults(this);
+	}
+
+	public void resetDatabase() {
+		FlowManager.getDatabase(MyDatabase.NAME).reset(this);
 	}
 }
