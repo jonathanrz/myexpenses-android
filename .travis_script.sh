39a13e830ce2b841788b3a204281233934a21092
Jonathan Zanella <jonathanrafaelz@gmail.com>
2016-08-07 18:57:12 -0300

    merge

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
+./gradlew assembleDebug
 ./gradlew connectedAndroidTest
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	./gradlew testfairyJonathan
-	./gradlew testfairyThainara
 fi
