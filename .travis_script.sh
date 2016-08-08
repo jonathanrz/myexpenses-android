4d3d924254ce588e9530faf32d8529a25902fa07
Jonathan Zanella <jonathanrafaelz@gmail.com>
2016-08-07 21:56:44 -0300

    Trying to solve travis ci problems

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-./gradlew -Pandroid.testInstrumentationRunnerArguments.class=br.com.jonathanzanella.myexpenses.AppNavigationTest connectedAndroidTest
+./gradlew connectedDebugAndroidTest
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	./gradlew testfairyJonathan
 fi
