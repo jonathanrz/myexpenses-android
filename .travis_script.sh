8c5146031ccdadfa4e1439e9a68bcbc20af4ece4
Jonathan Zanella <jonathanrafaelz@gmail.com>
2016-08-09 14:07:54 -0300

    trying to solve snap ci problems

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
+./gradlew assembleDebugAndroidTest
 ./gradlew -Pandroid.testInstrumentationRunnerArguments.class=br.com.jonathanzanella.myexpenses.AppNavigationTest connectedAndroidTest
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	./gradlew testfairyJonathan
