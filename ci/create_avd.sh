3c64df923a5389d27aa713943e2a010389e53b2e
Jonathan Zanella <jonathanrafaelz@gmail.com>
2016-08-23 10:21:08 -0300

    Create avd script

---
+++ ci/create_avd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# existance of this file indicates that all dependencies were previously installed, and any changes to this file will use a different filename.
+INITIALIZATION_FILE="$ANDROID_HOME/.create_avd-$(git log -n 1 --format=%h -- $0)"
+
+if [ ! -e ${INITIALIZATION_FILE} ]; then
+  echo no | android create avd --force -n test -t android-23 --abi armeabi-v7a #creates device
+
+  emulator -avd test -no-skin -no-audio -no-window & #starts emulator and put the process on background
+else
+  echo "not found"
+fi
