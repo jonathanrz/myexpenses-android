c89d7b5f78b409a3de10f75c24c284fa43d9fd30
Jonathan Zanella <jonathanrafaelz@gmail.com>
2016-08-25 19:21:17 -0300

    Correction for release build type

--- app/src/release/java/br/com/jonathanzanella/myexpenses/Environment.java
+++ app/src/release/java/br/com/jonathanzanella/myexpenses/Environment.java
@@ -1,6 +1,7 @@
 package br.com.jonathanzanella.myexpenses;
 
 import java.util.Locale;
+import br.com.jonathanzanella.myexpenses.user.Users;
 
 /**
  * Created by Jonathan Zanella on 07/02/16.
@@ -10,4 +11,7 @@ public interface Environment {
 	Locale PTBR_LOCALE = new Locale("pt_BR");
 	String SERVER_URL = "https://jonathanzanella-myexpenses.herokuapp.com/";
 	String CURRENT_USER = "Jonathan";
+	String CURRENT_USER_UUID = Users.JONATHAN;
+	long SYNC_PERIODIC_EXECUTION_FREQUENCY = 15L * 60L;
+	long SYNC_FLEX_EXECUTION = 2L * 60L;
 }
