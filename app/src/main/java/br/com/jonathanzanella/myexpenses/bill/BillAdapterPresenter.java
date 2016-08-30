bd798be41341c69bc085a3be80a87ebd2d68da65
Jonathan Zanella <jonathanrafaelz@gmail.com>
2016-08-30 00:13:04 -0300

    Bills Adapter Presenter

--- app/src/main/java/br/com/jonathanzanella/myexpenses/bill/BillAdapterPresenter.java
+++ app/src/main/java/br/com/jonathanzanella/myexpenses/bill/BillAdapterPresenter.java
@@ -1,5 +1,8 @@
 package br.com.jonathanzanella.myexpenses.bill;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,6 +15,7 @@ class BillAdapterPresenter {
 	private BillAdapter adapter;
 
 	private List<Bill> bills;
+	private List<Bill> billsFiltered;
 
 	BillAdapterPresenter(BillAdapter adapter, BillRepository repository) {
 		this.repository = repository;
@@ -21,16 +25,32 @@ class BillAdapterPresenter {
 
 	private void loadBills() {
 		bills = repository.userBills();
+		billsFiltered = bills;
 	}
 
 	List<Bill> getBills(boolean invalidateCache) {
 		if(invalidateCache)
 			loadBills();
-		return Collections.unmodifiableList(bills);
+		return Collections.unmodifiableList(billsFiltered);
 	}
 
 	void addBill(Bill bill) {
 		bills.add(bill);
+		if(bills != billsFiltered)
+			billsFiltered.add(bill);
 		adapter.notifyItemInserted(bills.size() - 1);
+	}
+
+	public void filter(String filter) {
+		if(filter == null || filter.compareTo("") == 0) {
+			billsFiltered = bills;
+			return;
+		}
+
+		billsFiltered = new ArrayList<>();
+		for (Bill bill : bills) {
+			if(StringUtils.containsIgnoreCase(bill.getName(), filter))
+				billsFiltered.add(bill);
+		}
 	}
 }
