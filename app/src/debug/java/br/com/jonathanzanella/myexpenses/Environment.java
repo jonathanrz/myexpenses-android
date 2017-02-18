e4da924b1300445a4c735dd5aa5145176f46eaf2
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-02-18 20:11:15 -0200

    Renaming db and reseting version

--- app/src/debug/java/br/com/jonathanzanella/myexpenses/Environment.java
+++ app/src/debug/java/br/com/jonathanzanella/myexpenses/Environment.java
@@ -4,17 +4,13 @@ import java.util.Locale;
 
 import br.com.jonathanzanella.myexpenses.user.Users;
 
-/**
- * Created by Jonathan Zanella on 07/02/16.
- */
 public interface Environment {
-	boolean IS_DEBUG = true;
 	Locale PTBR_LOCALE = new Locale("pt_BR");
 	String SERVER_URL = "http://192.168.0.12:3000/";
 	String CURRENT_USER = "Jonathan";
 	String CURRENT_USER_UUID = Users.JONATHAN;
 	long SYNC_PERIODIC_EXECUTION_FREQUENCY = 15L * 60L;
 	long SYNC_FLEX_EXECUTION = 2L * 60L;
-	String DB_NAME = "MyExpenses";
-	int DB_VERSION = 5;
+	String DB_NAME = "MyExpenses.db";
+	int DB_VERSION = 1;
 }
