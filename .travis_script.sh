72ba72239da84d1ff6f5a1963c3ede9d45180e53
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-03-03 12:56:37 -0300

    Adding travis ci files

---
+++ .travis_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ev
+
+./gradlew assembleDebug
+./gradlew connectedAndroidTest
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+	./gradlew testfairyJonathan
+	./gradlew testfairyThainara
+fi
