bbefb37db04b72e04a06139ec2a931b384476072
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-03-14 07:37:27 -0300

    Adding assembleDebug to try to solve time out errors in connectedAndroidTest

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ev
 
-./gradlew connectedAndroidTest
+./gradlew assembleDebug
+./gradlew connectedAndroidTest -PdisablePreDex
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	./gradlew testfairyJonathan
