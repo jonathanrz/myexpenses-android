0e9cad163ac5e93851d0df3fab43f7d451a372f8
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-03-14 23:20:05 -0300

    Making travis ci run tests by package too

--- .travis_script.sh
+++ .travis_script.sh
@@ -3,7 +3,14 @@ set -ev
 
 ./gradlew assembleDebug
 ./gradlew testDebugUnitTest
-./gradlew connectedAndroidTest -PdisablePreDex
+./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.app
+./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.account
+./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.bill
+./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.card
+./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.expense
+./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.receipt
+./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.resume
+./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.source
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	./gradlew testfairyJonathan
