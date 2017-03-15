d7c63892b01f61b5a9489ccd8427971a845e9e34
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-03-15 07:46:55 -0300

    Making travis ci run ui tests with  more packages at once

--- .travis_script.sh
+++ .travis_script.sh
@@ -3,14 +3,8 @@ set -ev
 
 ./gradlew assembleDebug
 ./gradlew testDebugUnitTest
-./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.app
-./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.account
-./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.bill
-./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.card
-./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.expense
-./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.receipt
-./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.resume
-./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.source
+./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.app, br.com.jonathanzanella.myexpenses.account, br.com.jonathanzanella.myexpenses.bill, br.com.jonathanzanella.myexpenses.card
+./gradlew connectedAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.package=br.com.jonathanzanella.myexpenses.expense, br.com.jonathanzanella.myexpenses.receipt, br.com.jonathanzanella.myexpenses.resume, br.com.jonathanzanella.myexpenses.source
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	./gradlew testfairyJonathan
