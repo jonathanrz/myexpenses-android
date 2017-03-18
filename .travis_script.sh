13e47d51ede110501150bf16afb749a7a2a74572
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-03-18 08:59:17 -0300

    Removing test fairy upload from travis ci

--- .travis_script.sh
+++ .travis_script.sh
@@ -10,8 +10,3 @@ set -ev
 ./gradlew connectedDebugAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.size=small
 ./gradlew connectedDebugAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.size=medium
 ./gradlew connectedDebugAndroidTest -PdisablePreDex  -Pandroid.testInstrumentationRunnerArguments.size=large
-
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-	./gradlew testfairyJonathan
-	./gradlew testfairyThainara
-fi
