4d40ce05b0978f93c871144a060744034f60945f
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-03-27 22:39:13 -0300

    Revert "Converting fields to kotlin"
    This reverts commit 65ec021c9ad3470abe497b893aaf3348f8edb87c.

---
+++ app/src/main/java/br/com/jonathanzanella/myexpenses/database/Fields.java
@@ -0,0 +1,48 @@
+package br.com.jonathanzanella.myexpenses.database;
+
+public enum Fields {
+	ID("id"),
+	NAME("name"),
+	TITLE("title"),
+	DESCRIPTION("description"),
+	UUID("uuid"),
+	USER_UUID("userUuid"),
+	SERVER_ID("serverId"),
+	CREATED_AT("createdAt"),
+	UPDATED_AT("updatedAt"),
+	AMOUNT("amount"),
+	BALANCE("balance"),
+	CREDITED("credited"),
+	CHARGED("charged"),
+	CHARGE_NEXT_MONTH("chargedNextMonth"),
+	IGNORE_IN_OVERVIEW("ignoreInOverview"),
+	IGNORE_IN_RESUME("ignoreInResume"),
+	DATE("date"),
+	DUE_DATE("due_date"),
+	INIT_DATE("init_date"),
+	END_DATE("end_date"),
+	INCOME("income"),
+	VALUE("value"),
+	VALUE_TO_SHOW_IN_OVERVIEW("valueToShowInOverview"),
+	ACCOUNT_TO_PAY_CREDIT_CARD("accountToPayCreditCard"),
+	ACCOUNT_TO_PAY_BILLS("accountToPayBills"),
+	ACCOUNT_UUID("accountUuid"),
+	BILL_UUID("billUuid"),
+	CHARGEABLE_UUID("chargeableUuid"),
+	CHARGEABLE_TYPE("chargeableType"),
+	SOURCE_UUID("sourceUuid"),
+	TYPE("type"),
+	REMOVED("removed"),
+	SYNC("sync");
+
+	private String fieldName;
+
+	Fields(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	@Override
+	public String toString() {
+		return fieldName;
+	}
+}
