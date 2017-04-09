8fda3b0c011156699081c884f488c12eab7ee43e
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-04-08 22:50:52 -0300

    Trying to solve flakiness of EditCardTest

--- app/src/androidTest/java/br/com/jonathanzanella/myexpenses/card/EditCardTest.java
+++ app/src/androidTest/java/br/com/jonathanzanella/myexpenses/card/EditCardTest.java
@@ -74,8 +74,8 @@ public class EditCardTest {
 		final String editExpenseTitle = getTargetContext().getString(R.string.edit_card_title);
 		matchToolbarTitle(editExpenseTitle);
 		onView(withId(R.id.act_edit_card_name)).perform(scrollTo()).check(matches(withText(card.getName())));
-		onView(withId(R.id.act_edit_card_account)).perform(scrollTo()).check(matches(withText(card.getAccount().getName())));
 		typeTextIntoView(R.id.act_edit_card_name, " changed");
+		onView(withId(R.id.act_edit_card_account)).perform(scrollTo()).check(matches(withText(card.getAccount().getName())));
 
 		clickIntoView(R.id.action_save);
 
