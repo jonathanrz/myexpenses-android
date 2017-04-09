e778144847f41fd48ca23d600b2ed4301bfe712c
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-04-08 23:07:24 -0300

    Solving pmd

--- app/src/main/java/br/com/jonathanzanella/myexpenses/sync/ServerData.java
+++ app/src/main/java/br/com/jonathanzanella/myexpenses/sync/ServerData.java
@@ -9,14 +9,14 @@ public class ServerData {
 	private static final String SERVER_URL = "ServerUrl";
 	private static final String SERVER_TOKEN = "ServerToken";
 
-	private SharedPreferences sharedPreferences;
+	private final SharedPreferences sharedPreferences;
 
 	public ServerData(Context context) {
 		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
 	}
 
 	@SuppressLint("ApplySharedPref")
-	public void updateInfo(String serverUrl, String serverToken) {
+	void updateInfo(String serverUrl, String serverToken) {
 		SharedPreferences.Editor editor = sharedPreferences.edit();
 		editor.putString(SERVER_URL, serverUrl);
 		editor.putString(SERVER_TOKEN, serverToken);
