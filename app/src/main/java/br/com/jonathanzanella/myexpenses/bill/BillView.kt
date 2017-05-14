6e180586b62b4e6b9b52b0f49e208eb84560bbbc
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-05-14 10:23:17 -0300

    Converting ViewBill to Kotlin and Anko

---
+++ app/src/main/java/br/com/jonathanzanella/myexpenses/bill/BillView.kt
@@ -0,0 +1,81 @@
+package br.com.jonathanzanella.myexpenses.bill
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import br.com.jonathanzanella.myexpenses.R
+import br.com.jonathanzanella.myexpenses.views.BaseView
+import br.com.jonathanzanella.myexpenses.views.anko.applyTemplateViewStyles
+import br.com.jonathanzanella.myexpenses.views.anko.recyclerView
+import org.jetbrains.anko.*
+import org.jetbrains.anko.design.floatingActionButton
+
+class BillView : BaseView {
+
+    private val ui = BillViewUI()
+    private var adapter: BillAdapter = BillAdapter()
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun init() {
+        addView(ui.createView(AnkoContext.Companion.create(context, this)))
+
+        ui.bills.adapter = adapter
+        ui.bills.layoutManager = GridLayoutManager(context, 2)
+        ui.bills.itemAnimator = DefaultItemAnimator()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQUEST_ADD_BILL -> if (resultCode == Activity.RESULT_OK)
+                refreshData()
+        }
+    }
+
+    override fun refreshData() {
+        super.refreshData()
+
+        adapter.refreshData()
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun filter(s: String) {
+        super.filter(s)
+        adapter.filter(s)
+        adapter.notifyDataSetChanged()
+    }
+
+    companion object {
+        val REQUEST_ADD_BILL = 1003
+    }
+}
+
+class BillViewUI: AnkoComponent<BillView> {
+    lateinit var bills : RecyclerView
+
+    override fun createView(ui: AnkoContext<BillView>) = with(ui) {
+        frameLayout {
+            bills = recyclerView { id = R.id.view_bills_list}
+            floatingActionButton {
+                id = R.id.view_bills_fab
+                onClick {
+                    val ctx = context
+                    val i = Intent(context, EditBillActivity::class.java)
+                    if (ctx is Activity) {
+                        ctx.startActivityForResult(i, BillView.REQUEST_ADD_BILL)
+                    } else {
+                        ctx.startActivity(i)
+                    }
+                }
+            }
+        }.applyRecursively(::applyTemplateViewStyles)
+    }
+}
