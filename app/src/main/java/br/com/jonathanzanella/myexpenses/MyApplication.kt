4fc3badeac3606b31110f7f9d9209838f84e43d5
Jonathan Rafael Zanella <jonathanrafaelz@gmail.com>
2017-09-02 20:45:18 -0300

    Add Crashlytics

--- app/src/main/java/br/com/jonathanzanella/myexpenses/MyApplication.kt
+++ app/src/main/java/br/com/jonathanzanella/myexpenses/MyApplication.kt
@@ -6,7 +6,10 @@ import android.arch.persistence.room.Room
 import android.content.Context
 import br.com.jonathanzanella.myexpenses.database.DB_NAME
 import br.com.jonathanzanella.myexpenses.database.MyDatabase
+import br.com.jonathanzanella.myexpenses.helpers.CrashlyticsReportingTree
+import com.crashlytics.android.Crashlytics
 import com.facebook.stetho.Stetho
+import io.fabric.sdk.android.Fabric
 import net.danlew.android.joda.JodaTimeAndroid
 import timber.log.Timber
 import timber.log.Timber.DebugTree
@@ -23,6 +26,9 @@ class MyApplication : Application() {
 
         if (BuildConfig.DEBUG) {
             Timber.plant(DebugTree())
+        } else {
+            Fabric.with(this, Crashlytics())
+            Timber.plant(CrashlyticsReportingTree())
         }
     }
 
