56515e8e92a4297bfa129cf53f1113f4084b994d
Jonathan Rafael Zanella <jonathanrafaelz@gmail.com>
2017-09-01 21:38:05 -0300

    Add timber and fix lint warnings in Chargeable classes

--- app/src/main/java/br/com/jonathanzanella/myexpenses/chargeable/ChargeableType.kt
+++ app/src/main/java/br/com/jonathanzanella/myexpenses/chargeable/ChargeableType.kt
@@ -4,10 +4,4 @@ enum class ChargeableType {
     ACCOUNT,
     DEBIT_CARD,
     CREDIT_CARD;
-
-    companion object {
-        fun getType(type: String): ChargeableType {
-            return ChargeableType.values().first { it.name == type }
-        }
-    }
 }
