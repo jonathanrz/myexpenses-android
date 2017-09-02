0a8356c98d49fbef2af66ee1c8b5ef59853313e3
Jonathan Rafael Zanella <jonathanrafaelz@gmail.com>
2017-09-02 12:08:11 -0300

    Add timber and fix lint warnings in Source classes

--- app/src/main/java/br/com/jonathanzanella/myexpenses/source/EditSourceActivity.kt
+++ app/src/main/java/br/com/jonathanzanella/myexpenses/source/EditSourceActivity.kt
@@ -5,13 +5,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.AppCompatEditText
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import br.com.jonathanzanella.myexpenses.R
 import br.com.jonathanzanella.myexpenses.validations.ValidationError
 import br.com.jonathanzanella.myexpenses.views.anko.*
 import org.jetbrains.anko.*
+import timber.log.Timber
 
 class EditSourceActivity : AppCompatActivity(), SourceContract.EditView {
     override val context = this
@@ -71,7 +71,7 @@ class EditSourceActivity : AppCompatActivity(), SourceContract.EditView {
     override fun showError(error: ValidationError) {
         when (error) {
             ValidationError.NAME -> ui.editName.error = getString(error.message)
-            else -> Log.e(this.javaClass.name, "Validation unrecognized, field:" + error)
+            else -> Timber.e("Validation unrecognized, field:" + error)
         }
     }
 
