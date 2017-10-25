a44d03381382b53d4d6606d94ea243e8078761a2
Jonathan Zanella <jonathanrafaelz@gmail.com>
2017-10-25 20:31:51 -0200

    Remove snapshot from test

--- app/src/androidTest/java/br/com/jonathanzanella/myexpenses/receipt/EditReceiptTest.java
+++ app/src/androidTest/java/br/com/jonathanzanella/myexpenses/receipt/EditReceiptTest.java
@@ -37,7 +37,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static br.com.jonathanzanella.myexpenses.helpers.UIHelper.clearAndTypeTextIntoView;
 import static br.com.jonathanzanella.myexpenses.helpers.UIHelper.clickIntoView;
 import static br.com.jonathanzanella.myexpenses.helpers.UIHelper.matchToolbarTitle;
-import static com.facebook.testing.screenshot.Screenshot.snapActivity;
 import static junit.framework.Assert.assertTrue;
 import static org.hamcrest.core.Is.is;
 
@@ -106,7 +105,5 @@ public class EditReceiptTest {
 
 		onView(withId(R.id.act_show_receipt_name)).check(matches(withText(receipt.getName())));
 		assertThat(repository.all().size(), is(1));
-
-		snapActivity(activityTestRule.getActivity()).record();
 	}
 }
