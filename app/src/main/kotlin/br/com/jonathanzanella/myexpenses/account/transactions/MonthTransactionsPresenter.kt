8a7538dfc17e83777d6b71f4f6bce67f961eaa96
jonathanrz <jonathanrafaelz@gmail.com>
2018-01-12 20:24:56 -0200

    Fix account (#265)
    * Fix account
    
    * Remove token

--- app/src/main/kotlin/br/com/jonathanzanella/myexpenses/account/transactions/MonthTransactionsPresenter.kt
+++ app/src/main/kotlin/br/com/jonathanzanella/myexpenses/account/transactions/MonthTransactionsPresenter.kt
@@ -3,6 +3,7 @@ package br.com.jonathanzanella.myexpenses.account.transactions
 import br.com.jonathanzanella.myexpenses.account.Account
 import br.com.jonathanzanella.myexpenses.bill.BillDataSource
 import br.com.jonathanzanella.myexpenses.expense.ExpenseDataSource
+import br.com.jonathanzanella.myexpenses.extensions.toSingleList
 import br.com.jonathanzanella.myexpenses.receipt.ReceiptDataSource
 import br.com.jonathanzanella.myexpenses.transaction.Transaction
 import io.reactivex.Flowable
@@ -12,12 +13,13 @@ import org.joda.time.DateTime
 class MonthTransactionsPresenter(val billDataSource: BillDataSource, val expenseDataSource: ExpenseDataSource, val receiptDataSource: ReceiptDataSource) {
     fun getAccountTransactions(account: Account, month: DateTime) : Flowable<List<Transaction>> {
         return expenseDataSource.accountExpenses(account, month)
+                .mergeWith(receiptDataSource.monthly(month, account).map { it.map { it as Transaction } })
+                .toSingleList()
                 .flatMap {
                     val list = it.toMutableList()
 
                     if (account.accountToPayBills)
                         list.addAll(billDataSource.monthly(month).blockingFirst())
-                    list.addAll(receiptDataSource.monthly(month, account))
 
                     Single.just(list.toList())
                 }.toFlowable()
